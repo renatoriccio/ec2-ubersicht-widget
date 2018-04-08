f0086ba89c2ffd3e8571b2d746c0e4ad8b4b9e43
Renato Riccio <mail>
2018-04-09 00:19:00 +0200

    Add support for defining linux user via ec2 tag

--- ec2.widget/helper.sh
+++ ec2.widget/helper.sh
@@ -5,7 +5,15 @@ source ec2.widget/config
 
 shell() {
     IP=`/usr/local/bin/aws ec2 describe-instances --instance-ids $1 --region $2  | grep PublicIpAddress | awk -F ":" '{print $2}' | sed 's/[",]//g'`
-    url=`printf 'ec2-user@%s' $IP`
+
+    USER_STR=`/usr/local/bin/aws ec2 describe-instances --instance-ids $1 --region $2 --query Reservations[*].Instances[].Tags --output=text | grep -i "^user\t"`
+    if [ $? -eq 0 ]
+    then
+       USER=`echo $USER_STR | cut -d " " -f2`
+    fi
+
+    url=`printf '%s@%s' $USER $IP`
+
     osascript -e 'tell application "Terminal" to do script "ssh -i '${KEY_PATH}' '${url}'"'
 }
 
